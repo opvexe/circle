359ae0aeb5d934d41cba1bad4b198622ea868986
SHUMIN <tao_shuminx@foxmail.com>
2021-11-06 11:33:51 +0800

    feat: 初始化项目

--- circle.go
+++ circle.go
@@ -16,7 +16,36 @@ limitations under the License.
 
 package circle
 
+import "context"
+
 const URL = "https://informationworld.zdjt.com/index.php/api/mytask/"
 
+// Query processing parameters.
+type Query struct {
+	URL     string
+	Page    int
+	Version string
+}
 
+type Task struct {
+	Title           string `json:"title"`
+	EndTime         int    `json:"end_time"`
+	ReadCount       int    `json:"read_count"`
+	FinishScore     int    `json:"finish_score"`
+	MicroURL        string `json:"micro_url"`
+	SltImg          string `json:"slt_img"`
 
+	CircleCountRw   int    `json:"circle_count_rw"`
+	GroupCountRw    int    `json:"group_count_rw"`
+	ID              int    `json:"id"`
+	UserTaskCount   int    `json:"user_task_count"`
+	UserReadCount   int    `json:"user_read_count"`
+	UserScore       int    `json:"user_score"`
+	UserCircleCount int    `json:"user_circle_count"`
+	UserGroupCount  int    `json:"user_group_count"`
+}
+
+// Tasker is an interface for gathering, dispose task.
+type Tasker interface {
+	Fetch(ctx context.Context, query Query) ([]Task, error)
+}
